51e8e3ff07fbf95fb91f9052ecce08669ce3c399
Mowanuola Akin-Jimoh <mowanuola.akinjimoh@GBN-MacBook-Pro.local>
2020-10-13 15:55:48 +0100

    Worked on product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class ProductService {
+private _albumUrl= '../assets/album.json';
+  constructor(private _http: Http) { }
+
+  getAlbum(id:number){
+    console.log(this._albumUrl)
+    return this._http.get(this._albumUrl).map((response)=>{
+      return response.json()
+    })
+  }
+}
